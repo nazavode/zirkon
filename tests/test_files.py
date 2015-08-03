d7713e88d69b4a1a5085171277329ea3ca2998b8
Simone Campagna <simone.campagna@tiscali.it>
2015-08-03 12:15:39 +0200

    utils.files tested

---
+++ tests/test_files.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import pytest
+
+from daikon.utils import files
+
+
+def test_createdir(tmpdir):
+    #assert os.path.exists(tmpdir.strpath)
+    #assert os.path.isdir(tmpdir.strpath)
+    tdir = os.path.join(tmpdir.strpath, 'xyz')
+    assert not os.path.exists(tdir)
+    ttdir = os.path.join(tdir, 'abc')
+    assert not os.path.exists(ttdir)
+    tfile = os.path.join(ttdir, 'a.txt')
+    files.createdir(tfile)
+    assert os.path.isdir(ttdir)
+    assert os.path.isdir(tdir)
