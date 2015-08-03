2d8c110637f43b6fe2a4bf07a749f3ab020c4aa1
Simone Campagna <simone.campagna@tiscali.it>
2015-08-03 12:22:41 +0200

    increased testing of ValidatorValidator

--- tests/test_validator_validator.py
+++ tests/test_validator_validator.py
@@ -18,6 +18,12 @@ def test_validator_validator_from_string_ok(validator_validator):
     validator_instance = validator_validator.validate(key='<key>', value=validator_string, defined=True, mode='load')
     assert isinstance(validator_instance, validator.IntTupleValidator)
 
+def test_validator_validator_from_string_err_no_load(validator_validator):
+    validator_string = "IntTuple(min_len=4, item_max=3, default=(0, 2, 1, 0, 2))"
+    with pytest.raises(TypeValidationError) as exc_info:
+        validator_instance = validator_validator.validate(key='<key>', value=validator_string, defined=True)
+    assert str(exc_info.value) == """<key>='IntTuple(min_len=4, item_max=3, default=(0, 2, 1, 0, 2))': invalid type str - expected type is Validator"""
+
 def test_validator_validator_from_string_err0(validator_validator):
     validator_string = "IntTuple(min_len=4, item_max=3, default=(0, 2, 1, 0, 2)"
     with pytest.raises(TypeValidationError) as exc_info:
@@ -29,3 +35,13 @@ def test_validator_validator_from_string_err1(validator_validator):
     with pytest.raises(TypeValidationError) as exc_info:
         validator_instance = validator_validator.validate(key='<key>', value=validator_string, defined=True, mode='load')
     assert str(exc_info.value) == """<key>=[2, 3, 4]: invalid type list - expected type is Validator"""
+
+def test_validator_validator_to_string(validator_validator):
+    validator_string = "IntTuple(min_len=4, item_max=3, default=(0, 2, 1, 0, 2))"
+    validator_instance = validator_validator.validate(key='<key>', value=validator_string, defined=True, mode='load')
+    assert isinstance(validator_instance, validator.IntTupleValidator)
+    validator_instance_string = validator_validator.validate(key='<key>', value=validator_instance, defined=True, mode='store')
+    validator_instance2 = validator_validator.validate(key='<key>', value=validator_instance_string, defined=True, mode='load')
+    assert isinstance(validator_instance2, validator.IntTupleValidator)
+    assert validator_instance == validator_instance2
+
