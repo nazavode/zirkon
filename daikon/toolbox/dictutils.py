d366cc6369d458d69919a9c9eb7f21218e1c3b8c
Simone Campagna <simone.campagna@tiscali.it>
2015-08-07 14:15:38 +0200

    added toolbox.dictutils

---
+++ daikon/toolbox/dictutils.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Simone Campagna
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+toolbox.dictutils
+=================
+Utility function for dictionaries.
+"""
+
+__author__ = "Simone Campagna"
+
+__author__ = "Simone Campagna"
+__all__ = [
+    'as_dict',
+    'standard_dict',
+    'compare_dicts',
+]
+
+import collections
+
+
+def as_dict(dct, *, depth=-1, dict_factory=dict):
+    """to_standard_dict(dct, *, depth=-1, dict_class=dict) -> dict object
+    """
+    stddct = dict_factory(dct)
+    dcts = [stddct]
+    while depth != 0 and dcts:
+        next_dcts = []
+        for dct in dcts:
+            for key, value in dct.items():
+                if isinstance(value, collections.Mapping):
+                    dct_value = dict_factory(value)
+                    dct[key] = dct_value
+                    next_dcts.append(dct_value)
+        dcts = next_dcts
+        if depth > 0:
+            depth -= 1
+    return stddct
+
+def compare_dicts(dct0, dct1):
+    """compare_dicts(dct0, dct1) -> True/False
+    """
+    stddct0 = as_dict(dct0, depth=-1, dict_factory=dict)
+    stddct1 = as_dict(dct1, depth=-1, dict_factory=dict)
+    return stddct0 == stddct1
+
+
---
+++ tests/test_dictutils.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import collections
+import os
+
+import pytest
+
+from daikon.toolbox import dictutils
+
+
+def test_as_dict():
+    dct = {}
+    stddct = dictutils.as_dict(dct)
+    assert stddct == dct
+    assert stddct is not dct
+
+def test_as_dict_content():
+    dct = {'a': 10, 'b': {'x': 1, 'y': {'r': 222}, 'z': 3}, 'c': 3}
+    stddct = dictutils.as_dict(dct)
+    assert stddct == dct
+    assert stddct is not dct
+
+def test_as_dict_content_conv():
+    dct = {'a': 10, 'b': collections.OrderedDict([('x', 1), ('y', collections.OrderedDict([('r', 222)])), ('z', 3)]), 'c': 3}
+    stddct = dictutils.as_dict(dct)
+    assert stddct == dct
+    assert stddct is not dct
+    assert type(stddct['b']) == dict
+    assert type(stddct['b']['y']) == dict
+
+def test_as_dict_content_conv_depth():
+    dct = {'a': 10, 'b': collections.OrderedDict([('x', 1), ('y', collections.OrderedDict([('r', 222)])), ('z', 3)]), 'c': 3}
+    stddct = dictutils.as_dict(dct, depth=1)
+    assert stddct == dct
+    assert stddct is not dct
+    assert type(stddct['b']) == dict
+    assert type(stddct['b']['y']) == collections.OrderedDict
+
+def test_as_dict_content_class():
+    dct = {'a': 10, 'b': {'x': 1, 'y': {'r': 222}, 'z': 3}, 'c': 3}
+    stddct = dictutils.as_dict(dct, dict_factory=collections.OrderedDict)
+    assert type(stddct) == collections.OrderedDict
+    assert type(stddct['b']) == collections.OrderedDict
+    assert type(stddct['b']['y']) == collections.OrderedDict
+
+def test_compare_dicts():
+    dct0 = {'a': 10, 'b': collections.OrderedDict([('x', 1), ('y', collections.OrderedDict([('r', 222)])), ('z', 3)]), 'c': 3}
+    dct1 = {'a': 10, 'b': {'x': 1, 'y': {'r': 222}, 'z': 3}, 'c': 3}
+    assert dictutils.compare_dicts(dct0, dct1)
+
+def test_compare_dicts_no_0():
+    dct0 = {'a': 10, 'b': collections.OrderedDict([('x', 1), ('y', collections.OrderedDict([('r', 221)])), ('z', 3)]), 'c': 3}
+    dct1 = {'a': 10, 'b': {'x': 1, 'y': {'r': 222}, 'z': 3}, 'c': 3}
+    assert not dictutils.compare_dicts(dct0, dct1)
+    assert not dictutils.compare_dicts(dct1, dct0)
+
+def test_compare_dicts_no_1():
+    dct0 = {'a': 10, 'b': collections.OrderedDict([('x', 1), ('z', 3)]), 'c': 3}
+    dct1 = {'a': 10, 'b': {'x': 1, 'y': {'r': 222}, 'z': 3}, 'c': 3}
+    assert not dictutils.compare_dicts(dct0, dct1)
+    assert not dictutils.compare_dicts(dct1, dct0)
