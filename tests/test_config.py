399e70c0e002bc2a61788bf128bc1f778ad3d15e
Simone Campagna <simone.campagna@tiscali.it>
2015-08-15 21:27:05 +0200

    tests added

--- tests/test_config.py
+++ tests/test_config.py
@@ -40,33 +40,37 @@ from daikon.config import Config, ROOT, SECTION
 from daikon.toolbox.serializer import JSONSerializer, \
     ConfigObjSerializer, PickleSerializer
 
-def test_Config_create_empty(string_io):
-    config = Config()
+@pytest.fixture(params=[True, False, {}])
+def defval(request):
+    return request.param
+
+def test_Config_create_empty(string_io, defval):
+    config = Config(defaults=defval)
     config.dump(stream=string_io)
     assert string_io.getvalue() == ""
 
-def test_Config_create_dictionary(generic_dictionary, string_io):
-    config = Config(dictionary=generic_dictionary)
+def test_Config_create_dictionary(generic_dictionary, string_io, defval):
+    config = Config(dictionary=generic_dictionary, defaults=defval)
     config.dump(stream=string_io)
     assert string_io.getvalue() == ""
 
-def test_Config_create_init(simple_config_content, string_io):
-    config = Config(init=simple_config_content)
+def test_Config_create_init(simple_config_content, string_io, defval):
+    config = Config(init=simple_config_content, defaults=defval)
     config.dump(stream=string_io)
     assert string_io.getvalue() == SIMPLE_SECTION_DUMP
 
-def test_Config_create_dictionary_init(dictionary, simple_config_content, string_io):
-    config = Config(dictionary=dictionary, init=simple_config_content)
+def test_Config_create_dictionary_init(dictionary, simple_config_content, string_io, defval):
+    config = Config(dictionary=dictionary, init=simple_config_content, defaults=defval)
     config.dump(stream=string_io)
     assert string_io.getvalue() == SIMPLE_SECTION_DUMP
     assert len(dictionary) > 0
 
-def test_Config_create_dictionary_init_overlap(string_io):
+def test_Config_create_dictionary_init_overlap(string_io, defval):
     dictionary = collections.OrderedDict()
     dictionary['x'] = 10
     dictionary['y'] = 10
     init = {'a': 20, 'y': 30}
-    config = Config(init, dictionary=dictionary)
+    config = Config(init, dictionary=dictionary, defaults=defval)
     assert len(config) == 3
     assert config['x'] == 10
     assert config['y'] == 30
@@ -123,8 +127,8 @@ def test_Config_get_serializer_configobj():
 def test_Config_get_serializer_pickle():
     assert isinstance(Config.get_serializer("pickle"), PickleSerializer)
 
-def test_Config_deferred():
-    config = Config()
+def test_Config_deferred(defval):
+    config = Config(defaults=defval)
     config['a'] = 10
     config['b'] = 20
     config['c'] = SECTION['a'] * SECTION['b']
@@ -139,8 +143,8 @@ def test_Config_deferred():
     assert isinstance(config['options']['e'], int)
     assert config['options']['e'] == 117
 
-def test_Config_deferred_error():
-    config = Config()
+def test_Config_deferred_error(defval):
+    config = Config(defaults=defval)
     config['a'] = 10
     with pytest.raises(KeyError) as exc_info:
         config['c'] = SECTION['a'] * SECTION['b']
