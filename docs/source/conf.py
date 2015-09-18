4953b53c80b1fdde4ca486205369e91c8093b41e
Simone Campagna <simone.campagna@tiscali.it>
2015-09-18 16:50:39 +0200

    read the docs theme applied

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,7 +102,15 @@ html_logo = "img/logo.png"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -356,9 +364,4 @@ epub_exclude_files = ['search.html']
 #def setup(app):
 #    app.connect("autodoc-skip-member", skip)
 
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
