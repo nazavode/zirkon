0b0839908cb011a397b25e53945a0c3d40b6c7ac
Simone Campagna <simone.campagna@tiscali.it>
2015-09-20 15:44:49 +0200

    zirkon module exports Config and Schema

--- zirkon/__init__.py
+++ zirkon/__init__.py
@@ -22,4 +22,14 @@ Configuration library.
 __author__ = "Simone Campagna"
 __copyright__ = 'Copyright (c) 2015 Simone Campagna'
 __license__ = 'Apache License Version 2.0'
-__all__ = []
+
+__all__ = [
+    'Config',
+    'Schema',
+    'ConfigValidationError',
+    'SECTION',
+    'ROOT',
+]
+
+from .config import Config, ConfigValidationError, SECTION, ROOT
+from .schema import Schema
